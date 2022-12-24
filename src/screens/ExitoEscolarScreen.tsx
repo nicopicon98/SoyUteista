02717205ad177bcd81d801c8f8fa0f26040a6516
David Gomez <davidgggomez0421@gmail.com>
2022-12-24 15:29:16 -0500

    Correcion radio UTS

--- src/screens/ExitoEscolarScreen.tsx
+++ src/screens/ExitoEscolarScreen.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import AppBarComponent from '../components/AppBarComponent';
 import Video from 'react-native-video';
 import {StyleSheet} from 'react-native';
-import lista from '../resources/aa.m3u';
 const ExitoEscolarScreen = () => {
-  const imageBackground: string = "../resources/aa.m3u";
+  const imageBackground: string = '../resources/aa.m3u';
   return (
     <>
       <AppBarComponent title="Exito Escolar" />
       <Video
-        source={
-         require(imageBackground)
-        }
+        source={{
+          uri: 'http://edge.teveo.com.co/live/AeAAAgAaAANCA1IAyADIVKwAAAAAAAAAAmOnOAqnAD7PAAAA/playlist.m3u8',
+        }}
         rate={1.0}
+        playInBackground
         volume={1.0}
         style={{width: 300, height: 300, marginTop: 100, borderWidth: 10}}
       />

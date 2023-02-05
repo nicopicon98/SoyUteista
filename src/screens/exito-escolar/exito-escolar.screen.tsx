efbe8666629ac6b9d08a8d4a0dde5ff79bfa2e3f
David Gómez <davidgggomez0421@gmail.com>
2023-02-05 11:01:48 -0500

    consumos terminados, falta mostrar tarjetas

--- src/screens/exito-escolar/exito-escolar.screen.tsx
+++ src/screens/exito-escolar/exito-escolar.screen.tsx
@@ -1,10 +1,70 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { AppBarComponent } from '@src/components/app-bar';
+import { Appearance, Dimensions, StyleSheet } from 'react-native';
+import { View } from 'react-native-animatable';
+import { ActivityIndicator } from 'react-native-paper';
+import { useVideosPodcast } from './hooks';
+import { colores } from '@src/theme';
+import CardPodcast from './components/CardPodcast';
+import CardVideo from './components/CardVideo';
+
+const Tab = createMaterialTopTabNavigator();
+const { width } = Dimensions.get('window');
 
 export const ExitoEscolarScreen = () => {
+  const { isLoading, podcast, videos } = useVideosPodcast()
+  const colorScheme = Appearance.getColorScheme();
   return (
     <>
       <AppBarComponent title="Exito Escolar" />
-      
+      <View style={{ flex: 1 }}>
+        {isLoading && <ActivityIndicator style={styles.loader} color={colores.Pantone_382_C} animating={isLoading} size='large' />}
+        {!isLoading &&
+          <Tab.Navigator
+            style={{
+              paddingTop: 0
+            }}
+            sceneContainerStyle={{
+              backgroundColor: colorScheme === 'dark' ? 'black' : 'white'
+            }}
+            screenOptions={() => ({
+              tabBarLabelStyle: {
+                fontSize: width * 0.04,
+                fontWeight: '700',
+              },
+              tabBarPressColor: colores.Pantone_382_C,
+              tabBarIndicatorStyle: {
+                backgroundColor: colorScheme === 'dark' ? 'white' : colores.Pantone_382_C
+              },
+              tabBarStyle: {
+                borderTopColor: colores.Cool_Gray_5_C,
+                borderTopWidth: 0,
+                elevation: 0,
+                shadowColor: 'transparent',
+                backgroundColor: colorScheme === 'dark' ? 'black' : 'white',
+              },
+              tabBarActiveTintColor: colorScheme === 'dark' ? 'white' : 'black'
+            })}
+          >
+
+            <Tab.Screen name="Podcasts">
+              {() => <CardPodcast podcast={podcast.data} />}
+            </Tab.Screen>
+            <Tab.Screen name="Videos">
+              {() => <CardVideo />}
+            </Tab.Screen>
+          </Tab.Navigator>
+        }
+      </View>
     </>
   );
 };
+
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- src/services/podcast.service.ts
+++ src/services/podcast.service.ts
@@ -1,7 +1,8 @@
 import { PodcastInterface } from './../screens/exito-escolar/models/get-podcasts.model';
 import { webserviceAPI } from "@src/api";
 
-export const getPodcastService = async () => {
-  const rep = await webserviceAPI.get<PodcastInterface>(`/podcast`);
+export const getPodcastService = async (email: string, API_KEY: string) => {
+  const rep = await webserviceAPI.get<PodcastInterface>(`/podcast/?email=${email}&key=${API_KEY}`);
+  console.log(rep.data)
   return rep
 }

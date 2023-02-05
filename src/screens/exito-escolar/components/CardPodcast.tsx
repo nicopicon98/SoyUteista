8e4fbe677d2e853fc525cac0d62e3656de051b79
David Gómez <davidgggomez0421@gmail.com>
2023-02-05 11:35:00 -0500

    podcast y videos terminados

--- src/screens/exito-escolar/components/CardPodcast.tsx
+++ src/screens/exito-escolar/components/CardPodcast.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import { FlatList, Image, ScrollView, Text } from 'react-native'
+import { Dimensions, FlatList, Image, Linking, ScrollView, Text, TouchableOpacity } from 'react-native'
 import { View } from 'react-native-animatable'
 import { Podcast } from '../models'
 
+const screenSize = Dimensions.get('screen');
 
 interface Props {
     podcast: Podcast[]
 }
 const CardPodcast = ({ podcast }: Props) => {
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
             <FlatList
                 data={podcast}
-                renderItem={({ item }) => <View style={{ borderWidth: 5 }}>
-                    <Image source={{uri: item.imagen}}/>
-                </View>}
+                renderItem={({ item }) =>
+                    <TouchableOpacity style={{
+                        marginVertical: screenSize.height * 0.01
+                    }}
+                        activeOpacity={0.7}
+                        onPress={() => {
+                            Linking.openURL(item.url)
+                        }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: screenSize.fontScale * 23 }}>{item.titulo}</Text>
+                        <Image source={{ uri: item.imagen }} style={{ height: screenSize.height * 0.3, width: screenSize.width * 0.95, marginVertical: screenSize.height * 0.01 }} />
+
+                    </TouchableOpacity>}
             />
         </View>
     )

13ccd76ee30e666c0fe6d170a1449a1728a8ddd3
unknown <nicolaspicon98@gmail.com>
2023-03-27 16:50:00 -0500

    Right after implementing new localUpdate hook

--- App.tsx
+++ App.tsx
@@ -1,35 +1,35 @@
-import { StackNavigator } from '@src/navigator/stack-navigator';
-import { useNotifications, useCheckUpdates } from '@src/hooks';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { SnackbarProvider } from '@src/context/snackbar';
-import { AuthProvider } from '@src/context/auth';
-import { Platform } from 'react-native';
+import {StackNavigator} from '@src/navigator/stack-navigator';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {SnackbarProvider} from '@src/context/snackbar';
+import {AuthProvider} from '@src/context/auth';
+import useLocalUpdate from './src/hooks/use-local-update.hook';
 
-export const AppState = ({ children }: { children: JSX.Element | JSX.Element[] }) => { //le puedo pasar children como un elmento jsx o como muchos
-  return (
-    <AuthProvider>
-      {children}
-    </AuthProvider>
-  )
-}
+export const AppState = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) => {
+  //le puedo pasar children como un elmento jsx o como muchos
+  return <AuthProvider>{children}</AuthProvider>;
+};
 const App = () => {
-  useCheckUpdates();
-  if (Platform.OS === 'android') {
-    useNotifications()
-  }
-  
+  const {renderUpdateModal, showUpdateModal} = useLocalUpdate();
+
   return (
-    <NavigationContainer>
-      <AppState>
-        <PaperProvider>
-          <SnackbarProvider>
-            <StackNavigator />
-          </SnackbarProvider>
-        </PaperProvider>
-      </AppState>
-    </NavigationContainer>
-  )
-}
+    <>
+      {showUpdateModal ? renderUpdateModal : <></>}
+      <NavigationContainer>
+        <AppState>
+          <PaperProvider>
+            <SnackbarProvider>
+              <StackNavigator />
+            </SnackbarProvider>
+          </PaperProvider>
+        </AppState>
+      </NavigationContainer>
+    </>
+  );
+};
 
 export default App;

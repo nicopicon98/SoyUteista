8456906bad2dafbc378eaa2ac3501e966c863c41
unknown <nicolaspicon98@gmail.com>
2023-05-06 22:59:53 -0500

    making small changes

--- src/screens/crear-cita-tutoria/crear-cita-tutoria.screen.tsx
+++ src/screens/crear-cita-tutoria/crear-cita-tutoria.screen.tsx
@@ -247,7 +247,6 @@ export const CrearCitaTutoriaScreen = ({navigation}: NavigationProps) => {
 
   // Click on submit to open confirmation modal
   const onSubmitFirstPart = async () => {
-    console.log('click on first part');
     setIsLoadingBtnContinuar(true);
     try {
       const tutorInfoResp = await onLoadInfoTutor(
@@ -289,8 +288,6 @@ export const CrearCitaTutoriaScreen = ({navigation}: NavigationProps) => {
 
   // Click on submit to insert tutoria
   const onSubmit = async (data: TFormData) => {
-    //click
-    console.log('click en crear tutorias');
     //we first set the loader so the user knows he clicked on it
     setClickInsertTutoria(true);
     //prepare the object to be sent
@@ -307,7 +304,6 @@ export const CrearCitaTutoriaScreen = ({navigation}: NavigationProps) => {
       fecha_tutoria: data.fecha_tutoria,
     };
     try {
-      console.log(obj);
       const insertResp: IInsertTutoriaResponse =
         await Tutorias.postInsertTutoria(obj);
       if (!insertResp.success) {
